7c2aa52b4711160784fa13d5b7937bd0255119cf
unknown <701tkdgns@inu.ac.kr>
2022-06-20 13:53:42 +0900

    0620

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css';
@@ -6,7 +6,20 @@ import './navbar.css';
 // BEM => Block Element Modifier : container__element--attribute
 // bem 참고사이트: https://nykim.work/15
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#about">About</a></p>
+    <p><a href="#service">Services</a></p>
+    <p><a href="#project">Projects</a></p>
+    <p><a href="#contact">Contact us</a></p>
+  </>
+)
+
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='gpt3__navbar'>
         <div className='gpt3__navbar-links'>
@@ -14,12 +27,26 @@ const Navbar = () => {
               <img src={logo} alt='logo' />
             </div>
             <div className='gpt3__navbar-links_container'>
-              <p><a href="#home">Home</a></p>
-              <p><a href="#about">About</a></p>
-              <p><a href="#service">Services</a></p>
-              <p><a href="#project">Projects</a></p>
-              <p><a href="#contact">Contact us</a></p>
+              <Menu />
             </div>
+        </div>
+        {/* <div className='gpt3__navbar-sing'>
+          <p>sign in</p>
+          <button type='button'> Sign up</button>
+        </div> */}
+        <div className='gpt3__navbar-menu'>
+          {toggleMenu
+            ? < RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+            :  <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu && (
+            <div className='gpt3__navbar-menu_container scale-up-center'>
+              <div className='gpt3__navbar-menu_container-links'>
+                <Menu />
+              </div>
+            </div>
+          )}
+
         </div>
     </div>
   )
