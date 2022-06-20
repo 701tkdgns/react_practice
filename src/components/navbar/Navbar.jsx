40b7135bbd3491182f44ecb30cefaf01682ab6e0
unknown <701tkdgns@inu.ac.kr>
2022-06-20 18:05:47 +0900

    0620

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ import './navbar.css';
 const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#about">About</a></p>
-    <p><a href="#service">Services</a></p>
-    <p><a href="#project">Projects</a></p>
-    <p><a href="#contact">Contact us</a></p>
+    <p><a href="#wgpt3">What is GPT3?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
   </>
 )
 
